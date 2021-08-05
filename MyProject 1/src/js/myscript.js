22259f5ff4992bf3ee6089d69e0ebf3b563d9cbc
JuliaVlas <86491222+JuliaVlas@users.noreply.github.com>
2021-08-05 11:30:52 -0500

    добавление анимаций и тд

--- MyProject 1/src/js/myscript.js
+++ MyProject 1/src/js/myscript.js
@@ -1,4 +1,7 @@
 "use strict"
+
+$(document).ready(function(){
+    
         window.onload = function () {
     document.body.classList.add('loaded_hiding');
     window.setTimeout(function () {
@@ -6,8 +9,37 @@
       document.body.classList.remove('loaded_hiding');
     }, 500);
   }
+        $(window).scroll(() => {
+            let scrollDistance = $(window).scrollTop();
+            $(".section").each((i,el) => {
+                if($(el).offset().top - $("nav").outerHeight() <= scrollDistance){
+                    $ ("nav a").each((i,el) => {
+                        if ($(el).hasClass("active")){
+                            $(el).removeClass("active"); }});
+                    $ ('nav li:eq ('+ i +')').find('a').addClass('active');
+                }
+            });
+        });
+    });
+let options = {threshold: [0.5]};
+let observer = new IntersectionObserver(onEntry, options);
+let elements = $('.element-animation');
+elements.each((i,el) => {
+    observer.observe(el);
+});
+function onEntry(entry){
+    entry.forEach( change => {
+        if (change.isIntersecting){
+            change.target.classList.add ('show-animation');
+            change.target.src = change.target.dataset.src;
+        }
+    })
+}
+ 
+        
         function calculate() {
-    site  = document.getElementById('site').value;
+   let site  = document.getElementById('site').value;
+    let day = 0;
 switch (site) {
    case "1000":
         day = 1;
@@ -22,7 +54,8 @@ switch (site) {
       site = 0;
       day = 0;
 }  
-design  = document.getElementById('design').value;
+let design  = document.getElementById('design').value;
+        let day1 = 0;
 switch (design) {
    case "1000":
         day1 = 1;
@@ -34,7 +67,8 @@ switch (design) {
       design = 0;
       day1 = 0;
 }
-      adaptive  = document.getElementById('adaptive').value;
+      let adaptive  = document.getElementById('adaptive').value;
+            let day2  = 0;
 switch (adaptive) {
    case "1000":
         day2 = 1;
@@ -54,3 +88,47 @@ switch (adaptive) {
             let days = +day + +day1 + +day2;
  document.getElementById('itogo1').innerHTML = days +" д.";
     }
+
+   var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlide() {
+    showSlides(slideIndex += 1);
+}
+function minusSlide() {
+    showSlides(slideIndex -= 1);  
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider-dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+$(document).ready(function() {
+  $('.image-link').magnificPopup({type:'image'});
+});
+$(document).ready(function(){
+ showStartModal();
+});
+function showStartModal(){
+    setTimeout(function(){
+     $('#staticBackdrop').modal('show');    
+    }, 3000)}; 
+
+
